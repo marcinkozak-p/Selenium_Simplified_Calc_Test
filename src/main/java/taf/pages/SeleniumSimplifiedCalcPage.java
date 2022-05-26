68eb66413c1e270dcf16b3afcdc81870b276d996
Marcin.Kozak <marcin_kozak@epam.com>
2022-05-26 12:24:07 +0200

    Updated selenium dependency version Added Cucumber dependency Added the skeleton of calc page Added base test to see if the config runs - OK

--- src/main/java/taf/pages/SeleniumSimplifiedCalcPage.java
+++ src/main/java/taf/pages/SeleniumSimplifiedCalcPage.java
@@ -1,4 +1,51 @@
 package taf.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class SeleniumSimplifiedCalcPage {
+
+    @FindBy(xpath = "")
+    private WebElement inputFirst;
+
+    @FindBy(xpath = "")
+    private WebElement inputSecond;
+
+    @FindBy(xpath = "")
+    private WebElement operationsDropdownList;
+
+    @FindBy(xpath = "")
+    private WebElement operationResult;
+
+    @FindBy(xpath = "")
+    private WebElement calculateButton;
+
+    public SeleniumSimplifiedCalcPage(WebDriver driver) {
+        PageFactory.initElements(driver,this);
+        driver.get("https://testpages.herokuapp.com/calculate.php");
+    }
+
+    public void clearInputs(){
+        inputFirst.clear();
+        inputSecond.clear();
+    }
+
+    public void calculate(){
+        calculateButton.click();
+    }
+
+    public void setInputFirst(String input){
+        inputFirst.sendKeys(input);
+    }
+
+    public void setInputSecond(String input){
+        inputSecond.sendKeys(input);
+    }
+
+    public String getCalculationResult(){
+        return operationResult.getText();
+    }
+
 }
--- src/test/java/SeleniumSimplifiedCalculatorUITest.java
+++ src/test/java/SeleniumSimplifiedCalculatorUITest.java
@@ -1,24 +1,43 @@
+import io.cucumber.java.en.Given;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import taf.pages.SeleniumSimplifiedCalcPage;
 
 public class SeleniumSimplifiedCalculatorUITest {
 
-    WebDriver webDriver;
+    WebDriver driver;
 
-    @BeforeClass(alwaysRun = true)
-    public void setup(){
+
+    public void initDriver() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
 
     }
 
-    @Test
-    public void test1(){
+    @BeforeClass(alwaysRun = true)
+    public void setup() {
+        initDriver();
+        SeleniumSimplifiedCalcPage simplifiedCalcPage = new SeleniumSimplifiedCalcPage(driver);
+    }
 
+    @Test
+    public void test1(String firstInput, String secondInput) {
+        System.out.println("Hello test 1!");
     }
 
     @Test
     public void testName() {
 
     }
+
+    @AfterClass(alwaysRun = true)
+    public void terminate() {
+        driver.quit();
+    }
+
 
 }
